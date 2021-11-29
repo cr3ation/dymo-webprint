915de0fba67bc50c6bf9295e5660fa169d78347f
Henrik Engström <henrik.engstrom@schibsted.com>
2021-11-29 23:10:44 +0100

    Added json parameters

--- app/app.py
+++ app/app.py
@@ -4,8 +4,8 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 import json
 import os
-import urllib.parse
-
+import subprocess as sp
+import urllib.request
 
 # Create webapp
 app = Flask(__name__)
@@ -24,18 +24,35 @@ class Print(Resource):
         if not json_data:
             # Nothing to return
             return(json.loads('{ "error": "no data in request" }'), 400)
-        
-        text1 = urllib.parse.quote(json_data["text1"])
-        text2 = urllib.parse.quote(json_data["text2"])
 
-        shell_command = '/usr/local/bin/dymoprint "{0}" "{1}"'.format(text1, text2)
+        # Get parameters from request
+        text1 = json_data["text1"] if "text1" in json_data else None
+        text2 = json_data["text2"] if "text2" in json_data else None
+        img_url = json_data["img_url"] if "img_url" in json_data else None
+        qr = json_data["qr"] if "qr" in json_data else None
 
-        print_output = os.popen(shell_command).read()
-        if print_output:
-            result = '{"message": "something went horrobly wrong!"}'
-            return(json.loads(result), 500)
-        print(print_output)
-        return(json.loads('{ "message": "True" }'))
+        # Build print command
+        print_command = '/usr/local/bin/dymoprint "{0}"'.format(text1)        
+        if not text1:
+            return(json.loads('{ "error": "text1 parameter missing in request" }'), 400)
+        print_command = '{0} {1}'.format(print_command, text1)
+        if text2:
+            print_command = '{0} "{1}"'.format(print_command, text2)
+        if qr:
+            print_command = '{0} --qr "{1}"'.format(print_command, qr)
+        if img_url:
+            img_path = os.path.dirname(os.path.abspath(__file__)) + "/img.jpg"
+            if os.path.exists(img_path):
+                os.remove(img_path)
+            urllib.request.urlretrieve(img_url, img_path)
+            print_command = '{0} --picture "{1}"'.format(print_command, img_path)
+ 
+        # Print
+        output = sp.getoutput(print_command)
+
+        # Return
+        info = '{ "message": "%s}" }' % output
+        return(json.loads(info))
 
 
 class HelloWorld(Resource):
